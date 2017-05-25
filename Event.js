f7e113f550a30644f8e7334c1ba178a551890197
lijun <420695598@qq.com>
2017-05-25 14:58:33 +0800

    clone change

--- Event.js
+++ Event.js
@@ -1,21 +1,7 @@
 'use strict';
 
 let Event = {
-    PLAY_STATUS: 'PLAY_STATUS',
-    //video playing
-    VIDEO_PLAY: 'VIDEO_PLAY',
-    //video stop
-    VIDEO_STOP: 'VIDEO_STOP',
-    //multi videos
-    MULTI_VIDEOS: 'MULTI_VIDEOS',
-    //stop stream
-    ANCHOR_STOP: 'ANCHOR_STOP',
-    //no video
-    NO_VIDEO: 'NO_VIDEO',
-    //flv url unavailable
-    FLV_URL_UNAVAILABLE: 'FLV_URL_UNAVAILABLE',
-    //html5 player error
-    H5_PLAYER_ERROR: 'H5_PLAYER_ERROR'
+
 };
 
 let Status = {
